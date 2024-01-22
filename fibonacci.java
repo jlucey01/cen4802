4fb2a761ec279fee3eb4742b528f9366e39d2e2a
jlucey01 <jlucey@jm-pages.com>
2024-01-22 11:17:30 -0500

    JavaDoc update
    File has been updated with JavaDoc notation

--- fibonacci.java
+++ fibonacci.java
@@ -1,13 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class fibonacci {
-    public static void main(String[] args) {
+/**
+ * The fibonacci class provides the methods to calculate the nth term in the fibonacci sequence using recursion.
+ * @author      John Lucey
+ * @version     1.0
+ */
 
+
+
+public class fibonacci {
+    public static void main(String[] args ) {
+
+        /**
+         * Calls the calc method using 10 as the passed argument of 10 and generates result.
+         */
+
+        // The variable passed as argument
         int num = 10;
+
+        // Used to calculate the nth term
         int nth = calc(num);
 
-        System.out.println("The " + num + "th term of the Fibonacci sequence is " + nth + ".");
+        System.out.println("The " + num + "th term of the fibonacci sequence is " + nth + ".");
     }
+
+
+
+    /**
+     * Calculates the nth term in the Fibonacci sequence using recursion.
+     *
+     * @param num Tracks the position of the term in the fibonacci sequence.
+     * @return Provide the nth term in the fibonacci sequence.
+     */
 
     public static int calc(int num) {
         if (num <= 1) {
